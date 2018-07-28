125a4d2120b36d60629abebd3ea9f1cc6f7f29bb
Semir Tursunovic <tursunovic.semir@gmail.com>
2018-07-28 21:00:17 +0200

    Fix bux in USART initialization. Read data from USART2 using IRQ

--- lib/usart.c
+++ lib/usart.c
@@ -1,5 +1,15 @@
 #include "usart.h"
 
+/* Memory organization BUFFER */
+/* |valid_command|circular buffer| */
+/*   64 Byte       448 Byte */
+
+volatile uint8_t g_usart2_rx_buffer[USART2_BUFFER_SIZE] = {0};
+volatile uint16_t g_usart2_tx_wcnt = MAX_COMMAND_LENGHT + 1;
+volatile uint16_t g_usart2_tx_rcnt = MAX_COMMAND_LENGHT + 1;
+volatile uint16_t g_usart2_tx_cmdcnt = 0; //for creating valid command string & enables backspace
+
+
 void usart2_init(uint16_t baudrate)/*{{{*/
 {
   //enable clock for USART
@@ -7,9 +17,9 @@ void usart2_init(uint16_t baudrate)/*{{{*/
   //enable clock for Tx and Rx pins
   RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
   GPIOA->CRL &= ~(GPIO_CRL_MODE2_0 | GPIO_CRL_MODE2_1 | GPIO_CRL_CNF2_0 | GPIO_CRL_CNF2_1 |
-                  GPIO_CRL_MODE3_0 | GPIO_CRL_MODE3_1 | GPIO_CRL_CNF3_0 | GPIO_CRL_CNF3_1);
+      GPIO_CRL_MODE3_0 | GPIO_CRL_MODE3_1 | GPIO_CRL_CNF3_0 | GPIO_CRL_CNF3_1);
   GPIOA->CRL |= (GPIO_CRL_MODE2_0 | GPIO_CRL_MODE2_1 | GPIO_CRL_CNF2_1); //TX(alt - push_pull - 50mhz)
-  GPIOA->CRL |= (GPIO_CRL_CNF2_0); //RX(floating input)
+  GPIOA->CRL |= (GPIO_CRL_CNF3_0); //RX(floating input)
 
   //USART CONFIGURATION:
   //  - 8 bit data
@@ -28,6 +38,7 @@ void usart2_tx_byte(uint8_t data)/*{{{*/
 }/*}}}*/
 uint8_t usart2_rx_byte(void)/*{{{*/
 {
+
   while (!(USART2->SR & USART_SR_RXNE))
   {
     // waitin to receive data
@@ -35,4 +46,67 @@ uint8_t usart2_rx_byte(void)/*{{{*/
   return USART2->DR;
 }/*}}}*/
 
+void usart2_irq_enable(void)/*{{{*/
+{
+  USART2->CR1 &= ~(USART_CR1_UE);
+  NVIC_EnableIRQ(USART2_IRQn);
+  USART2->CR1 |= (USART_CR1_UE) | (USART_CR1_RXNEIE);
 
+}/*}}}*/
+void usart2_irq_disable(void)/*{{{*/
+{
+  USART2->CR1 &= ~( (USART_CR1_UE) | (USART_CR1_RXNEIE)  );
+  NVIC_DisableIRQ(USART2_IRQn);
+  USART2->CR1 |= (USART_CR1_UE);
+
+}/*}}}*/
+void USART2_IRQHandler(void)/*{{{*/
+{
+  g_usart2_rx_buffer[g_usart2_tx_wcnt] = USART2->DR;
+  ++g_usart2_tx_wcnt;
+  g_usart2_tx_wcnt >= USART2_BUFFER_SIZE  && (g_usart2_tx_wcnt = MAX_COMMAND_LENGHT + 1); //reset g_rx_wcnt
+}/*}}}*/
+uint8_t * usart2_read_rx_buffer(void)/*{{{*/
+{
+  volatile  uint8_t data = 0;
+  if((g_usart2_tx_cmdcnt == 0) && (*g_usart2_rx_buffer != '\n'))
+  {
+    *g_usart2_rx_buffer = '\n';
+    print("> ");
+  }
+  if(g_usart2_tx_rcnt != g_usart2_tx_wcnt) // chech if new data is available
+  {
+    data = g_usart2_rx_buffer[g_usart2_tx_rcnt];
+    ++g_usart2_tx_rcnt;
+    g_usart2_tx_rcnt >= USART2_BUFFER_SIZE  && (g_usart2_tx_rcnt = MAX_COMMAND_LENGHT + 1); //reset g_rx_rcnt
+    if(data == 0x08 || data == 0x7F)
+    {
+      if(g_usart2_tx_cmdcnt)
+      {
+        usart2_tx_byte('\b'); // backspace
+        usart2_tx_byte(' '); // delete
+        usart2_tx_byte('\b'); // backspace
+        g_usart2_rx_buffer[--g_usart2_tx_cmdcnt] = '\n';
+      }
+      else
+      {
+        *g_usart2_rx_buffer = '\n';
+      }
+    }
+    else if(data == '\n' || data == '\r' || g_usart2_tx_cmdcnt >= (MAX_COMMAND_LENGHT - 1))
+    {
+      usart2_tx_byte('\n');
+      usart2_tx_byte('\r');
+      g_usart2_rx_buffer[g_usart2_tx_cmdcnt] = '\0';
+      g_usart2_rx_buffer[MAX_COMMAND_LENGHT] = '\0'; // command end indicator
+      g_usart2_tx_cmdcnt = 0;
+      return g_usart2_rx_buffer;
+    }
+    else
+    {
+      USART2->DR = data; //show insterted characters to user
+      g_usart2_rx_buffer[g_usart2_tx_cmdcnt++] = data;
+    }
+  }
+  return 0;
+}/*}}}*/
--- lib/usart.h
+++ lib/usart.h
@@ -16,6 +16,9 @@
 #define USART_BAUDRATE_9600                       0x0ea6
 #define USART_BAUDRATE_115200                     0x0138
 
+#define USART2_BUFFER_SIZE 512
+#define MAX_COMMAND_LENGHT 64
+
 /*! \brief Initialize USART2 on STM32F1xx 
  *
  * \param  baudrate Set USART baudrate. 
@@ -38,5 +41,36 @@ void usart2_tx_byte(uint8_t data);
  */
 uint8_t usart2_rx_byte(void);
 
+/*! \brief Enable IRQ for USART2 
+ *
+ *  Generate IRQ for RXNE (RX not empty).
+ * \return No return value
+ */
+void usart2_irq_enable(void);
+
+/*! \brief Disable IRQ for USART2 
+ *
+ *  Don't generate IRQ for RXNE (RX not empty).
+ * \return No return value
+ */
+void usart2_irq_disable(void);
+
+/*! \brief USART2 IRQ handler function 
+ *
+ * Function called when USART2 generates IRQ request.
+ * \return No return value
+ */
+void USART2_IRQHandler(void);
+
+/*! \brief Read content of the rx buffer 
+ *
+ * This function prints content of the rx buffer to the users screen. 
+ * When ENTER key is pressed this function returns string which was
+ * entered by the user via keyboard.
+ *
+ * This function should be called frequently.
+ * \return 0 or entered string
+ * */
+uint8_t * usart2_read_rx_buffer(void);
 
 #endif /* end of include guard: USART_H_RL1KETST */
