05d3f6b7efb2fb54b72b6e5f1430a6e68d3a3fe6
Semir Tursunovic <tursunovic.semir@gmail.com>
2018-07-29 19:16:44 +0200

    Color scheme for print() function

---
+++ examples/sim800/color.h
@@ -0,0 +1,28 @@
+#ifndef _COLOR_H
+#define _COLOR_H 
+
+#define BRIGHT        "\x1B[1m"
+#define DIM           "\x1B[1m"
+#define CLEAN_DISPLAY "\x1B[2J"
+
+#define BLACK         "\x1B[30m"
+#define RED           "\x1B[31m"
+#define GREEN         "\x1B[32m"
+#define YELLOW        "\x1B[33m"
+#define BLUE          "\x1B[34m"
+#define MAGENTA       "\x1B[35m"
+#define CYAN          "\x1B[36m"
+#define WHITE         "\x1B[37m"
+#define RESET         "\x1B[0m"
+
+#define B_BLACK       "\x1B[40m"
+#define B_RED         "\x1B[41m"
+#define B_GREEN       "\x1B[42m"
+#define B_YELLOW      "\x1B[43m"
+#define B_BLUE        "\x1B[44m"
+#define B_MAGENTA     "\x1B[45m"
+#define B_CYAN        "\x1B[46m"
+#define B_WHITE       "\x1B[47m"
+
+#endif /* ifndef _COLOR_H */
+
